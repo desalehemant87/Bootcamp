3c9906d908fdbd76da71d51b4237e87f73f4ec70
Hemant Desale <desalehemant87@gmail.com>
2025-10-13 22:44:20 +0530

    Updated class10 assignment

---
+++ Class10_ECS+Terraform-pure-dynamic-code+multi-env-deployment+autoscaling-bits/Class10_Assignment/network.tf
@@ -0,0 +1,261 @@
+## 2 kinds of dependencies in terraform
+# implicit dependencies ->  aws_vpc.main.id
+# explicit dependencies -> depends_on = [aws_internet_gateway.main]
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.environment}-vpc"
+    Project = var.project_name
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" { 
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+# Public Subnet 1
+resource "aws_subnet" "public_sub1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "${var.subnet_cidrs[0]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.id}a"
+  tags = {
+    Name = "${var.environment}-public-sub1"
+  }
+}
+
+# Public Subnet 2
+resource "aws_subnet" "public_sub2" { 
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "${var.subnet_cidrs[1]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.id}b"
+  tags = {
+    Name = "${var.environment}-public-sub2"
+  }
+}
+
+# To ensure proper ordering, it is recommended to add an explicit dependency
+# on the Internet Gateway for the VPC.
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+    depends_on = [aws_internet_gateway.igw]
+}
+
+# Associate Public Subnet 1 with Public Route Table
+resource "aws_route_table_association" "public_sub1_association" {  
+  subnet_id      = aws_subnet.public_sub1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+# Associate Public Subnet 2 with Public Route Table
+resource "aws_route_table_association" "public_sub2_association" {  
+  subnet_id      = aws_subnet.public_sub2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Elastic IP 1 for NAT Gateway 1
+resource "aws_eip" "nat_eip1" {
+  tags = {
+    Name = "${var.environment}-nat-eip1"
+  }
+}
+
+# Elastic IP 2 for NAT Gateway 2
+resource "aws_eip" "nat_eip2" {
+  tags = {
+    Name = "${var.environment}-nat-eip2"
+  }
+}
+
+#NAT Gateway in Public Subnet 1 
+resource "aws_nat_gateway" "nat_gw1" {
+  allocation_id = aws_eip.nat_eip1.id
+  subnet_id     = aws_subnet.public_sub1.id
+  tags = {
+    Name = "${var.environment}-nat-gw1"
+  }
+  depends_on = [aws_internet_gateway.igw, aws_route_table.public_rt]
+}
+
+#NAT Gateway in Public Subnet 2
+resource "aws_nat_gateway" "nat_gw2" {
+  allocation_id = aws_eip.nat_eip2.id
+  subnet_id     = aws_subnet.public_sub2.id
+  tags = {
+    Name = "${var.environment}-nat-gw2"
+  }
+  depends_on = [aws_internet_gateway.igw, aws_route_table.public_rt]
+}
+
+# Private Subnet 1
+resource "aws_subnet" "private_sub1" {  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${var.subnet_cidrs[2]}"
+  availability_zone = "${data.aws_region.current.id}a"
+  tags = {
+    Name = "${var.environment}-private-sub1"
+  }
+}
+
+# Private Subnet 2
+resource "aws_subnet" "private_sub2" {  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${var.subnet_cidrs[3]}"
+  availability_zone = "${data.aws_region.current.id}b"
+  tags = {
+    Name = "${var.environment}-private-sub2"
+  }
+} 
+
+# Private Route Table for private subnet 1
+resource "aws_route_table" "private_rt1" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment}-private-rt1"
+  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw1.id
+  }
+    depends_on = [aws_nat_gateway.nat_gw1]
+}
+
+# Private Route Table for private subnet 2
+resource "aws_route_table" "private_rt2" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.environment}-private-rt2"
+  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw2.id
+  }
+    depends_on = [aws_nat_gateway.nat_gw2]
+}
+
+# Associate Private Subnet 1 with Private Route Table
+resource "aws_route_table_association" "private_sub1_association" {  
+  subnet_id      = aws_subnet.private_sub1.id
+  route_table_id = aws_route_table.private_rt1.id
+}
+
+# Associate Private Subnet 2 with Private Route Table
+resource "aws_route_table_association" "private_sub2_association" {  
+  subnet_id      = aws_subnet.private_sub2.id
+  route_table_id = aws_route_table.private_rt2.id
+}
+
+# 2 private subnet for database
+# RDS(DB) Subnet 1
+resource "aws_subnet" "rds_sub1" {  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${var.subnet_cidrs[4]}"
+  availability_zone = "${data.aws_region.current.id}a"
+  tags = {
+    Name = "${var.environment}-rds-sub1"
+  }
+} 
+
+# RDS(DB) Subnet 2
+resource "aws_subnet" "rds_sub2" {  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "${var.subnet_cidrs[5]}"
+  availability_zone = "${data.aws_region.current.id}b"
+  tags = {
+    Name = "${var.environment}-rds-sub2"
+  }
+}
+
+# ALB security group for load balancer -> inbound on port 80(http) and 443(for https) from internet(everywhere)
+resource "aws_security_group" "alb_sg" {  
+  name        = "${var.environment}-${var.app_name}-alb-sg"
+  description = "Security group for ALB - allows inbound traffic on port 80 and 443 from the world"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = {
+        Name = "${var.environment}-${var.app_name}-alb-sg"
+    }
+}
+
+# ECS security group --> inbound on port 8000 from load balancer(alb) only
+resource "aws_security_group" "ecs_service_sg" {  
+  name        = "${var.environment}-${var.app_name}-ecs-sg"
+  description = "Security group for ECS - allows inbound traffic from ALB SG on port 8000"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = var.ecs_app_values["container_port"]
+    to_port         = var.ecs_app_values["container_port"]
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]  # allow from alb sg only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = {
+        Name = "${var.environment}-${var.app_name}-ecs-sg"
+    }
+}
+
+# RDS security group -> inbound on port 5432 from ecs only
+resource "aws_security_group" "rds_sg" {  
+  name        = "${var.environment}-${var.app_name}-rds-sg"
+  description = "Security group for RDS - allows MySQL access from ecs tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ecs_service_sg.id]  # allow from ecs service sg only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = {
+        Name = "${var.environment}-${var.app_name}-rdsdb-sg"
+    }
+}
---
+++ Class10_ECS+Terraform-pure-dynamic-code+multi-env-deployment+autoscaling-bits/Class10_Assignment/variables.tf
@@ -0,0 +1,86 @@
+variable "region" {
+  description = "The region in which the resources will be created"
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "august-studentportal"
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  default     = "studentportal"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "The environment in which the resources will be created"
+  #default     = "dev"
+}
+
+variable "prefix" {
+  description = "main"
+  default = "main"
+}
+
+variable "subnet_cidrs" {
+  description = "A list of CIDR blocks for the subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+
+}
+
+variable "db_default_settings" {
+  type = any
+  default = {
+    allocated_storage       = 30
+    max_allocated_storage   = 50
+    engine_version          = 14.15
+    instance_class          = "db.t3.micro"
+    backup_retention_period = 2
+    db_name                 = "postgres"
+    ca_cert_name            = "rds-ca-rsa2048-g1"
+    db_admin_username       = "postgres"
+  }
+}
+
+variable "public_hosted_zone_id" {
+  description = "The hosted zone id for the public domain"
+  default     = "hemantdesale.tech"
+}
+
+variable "domain_name" {
+  description = "hemantdesale.tech"
+  default = "hemantdesale.tech"
+}
+
+variable "ecs_app_values" {
+  type        = map(string)
+  description = "values for ecs application"
+
+  default = {
+    container_name = "studentportal"
+    container_port = "8000"
+    image_name     = "ecs-studentportal-1.0"
+    cpu            = "256"
+    memory         = "512"
+    desired_count  = "2"
+    launch_type    = "FARGATE"
+    domain_name    = "hemantdesale.tech"
+    subdomain_name = "studentportal"
+  }
+
+}
